bcad9963458470094508313fc8a5a957854e57a9
tackyang <tackyang@tencent.com>
2023-12-11 20:33:40 +0800

    add day2 part1

--- src/adv2023/day2/mod.rs
+++ src/adv2023/day2/mod.rs
@@ -5,3 +5,17 @@ mod part2;
 const RED_NUM: usize = 12;
 const GREEN_NUM: usize = 13;
 const BLUE_NUM: usize = 14;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use color_eyre::Result;
+
+    #[test]
+    fn day2_part1_test_should_work() -> Result<()> {
+        let res = part1::solve("src/adv2023/day2/test1.txt".into())?;
+        println!("part1 res: {}", res);
+        assert_eq!(res, 8);
+        Ok(())
+    }
+}
