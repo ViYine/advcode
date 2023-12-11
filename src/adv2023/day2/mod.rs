1d5356eb1b9e4b1def114dd7c9772de3b03b0267
tackyang <tackyang@tencent.com>
2023-12-11 20:29:47 +0800

    add day2 part1

---
+++ src/adv2023/day2/mod.rs
@@ -0,0 +1,7 @@
+mod part1;
+mod part2;
+
+// 12 red cubes, 13 green cubes, and 14 blue cubes
+const RED_NUM: usize = 12;
+const GREEN_NUM: usize = 13;
+const BLUE_NUM: usize = 14;
---
+++ src/adv2023/day2/part2.rs
@@ -0,0 +1,5 @@
+use std::path::PathBuf;
+#[allow(dead_code)]
+pub(crate) fn solve(_input_path: PathBuf) -> color_eyre::Result<usize> {
+    todo!("day 2 part 2 not implemented")
+}
